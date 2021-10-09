c4b3218bdf94bf754ee574cd2d5e0e60705907bd
Muhamad Farid <muhammadfarid.alfachri@gmail.com>
2021-10-10 04:59:45 +0700

    Update mainconfig.php

--- mainconfig.php
+++ mainconfig.php
@@ -2,6 +2,6 @@
 $config['web'] = array(
 	'maintenance' => 0, // 1 = yes, 0 = no
 	'title' => 'Testing',
-	'base_url' => 'https://localhost/php-login/'
+	'base_url' => 'http://pro.mbahtekno.net/'
 );
 ?>
