a5c3b8e0e2250aab486c18452304c9b268da2dad
Muhamad Farid <muhammadfarid.alfachri@gmail.com>
2021-10-12 15:14:03 +0700

    Update userinfo.php

--- userinfo.php
+++ userinfo.php
@@ -54,11 +54,10 @@ require 'mainconfig.php';
                             var obj = JSON.parse(datas);
                             
                             if(obj.status===200){
-                              let str = `Name: ${obj.user.name}\nmail: ${obj.user.email}`;
                               Swal.fire({
                                 icon: 'success',
                                 title: 'Success!',
-                                text: str
+                                text: result
                               });
                             }else{
                               Swal.fire({
