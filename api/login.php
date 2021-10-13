ee8faad32d974fde60a6f5b69b000594ef30ed36
Muhamad Farid <muhammadfarid.alfachri@gmail.com>
2021-10-13 12:56:28 +0700

    Update login.php

--- api/login.php
+++ api/login.php
@@ -12,6 +12,26 @@ function msg($success,$status,$message,$extra = []){
     ],$extra);
 }
 
+function get_client_ip() {
+	$ipaddress = '';
+	if (getenv('HTTP_CLIENT_IP')) {
+		$ipaddress = getenv('HTTP_CLIENT_IP');
+	} elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+		$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+	} elseif (getenv('HTTP_X_FORWARDED')) {
+		$ipaddress = getenv('HTTP_X_FORWARDED');
+	} elseif (getenv('HTTP_FORWARDED_FOR')) {
+		$ipaddress = getenv('HTTP_FORWARDED_FOR');
+	} elseif (getenv('HTTP_FORWARDED')) {
+		$ipaddress = getenv('HTTP_FORWARDED');
+	} elseif (getenv('REMOTE_ADDR')) {
+		$ipaddress = getenv('REMOTE_ADDR');
+	} else {
+		$ipaddress = 'UNKNOWN';
+	}
+	return $ipaddress;
+}
+
 require __DIR__.'/../classes/Database.php';
 require __DIR__.'/../classes/JwtHandler.php';
 require __DIR__.'/model.php';
