4224289ef128c628c4a039209608f8fb1543f33a
Muhamad Farid <muhammadfarid.alfachri@gmail.com>
2022-04-21 17:15:39 +0700

    Update mainconfig.php

--- mainconfig.php
+++ mainconfig.php
@@ -2,6 +2,6 @@
 $config['web'] = array(
 	'maintenance' => 0, // 1 = yes, 0 = no
 	'title' => 'TEST',
-	'base_url' => 'http://pro.mbahtekno.net/'
+	'base_url' => 'https://mbahtekno.herokuapp.com/'
 );
 ?>
